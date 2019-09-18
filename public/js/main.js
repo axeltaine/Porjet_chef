ddc520c684e8b66a47b6a8fd5608e1a69ed48cdd
axeltaine <axeltaine@live.fr>
2019-09-18 15:43:54 +0200

    back to top

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,16 @@ function toggleDarkLight() {
   }, 1000)
 };
 
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#myBtn').fadeIn(); 
+      } else { 
+          $('#myBtn').fadeOut(); 
+      } 
+  }); 
+  $('#myBtn').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
