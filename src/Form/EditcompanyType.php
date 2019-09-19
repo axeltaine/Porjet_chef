038e584a0293f722e7c710f6517cec0baf13bf29
axeltaine <axeltaine@live.fr>
2019-09-19 16:00:38 +0200

    front form company

--- src/Form/EditcompanyType.php
+++ src/Form/EditcompanyType.php
@@ -6,17 +6,28 @@ use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class EditcompanyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Name_company')
-            ->add('Name_director')
-            ->add('Siret_company')
-            ->add('Phone_company')
-            ->add('Email_company')
+        ->add('name_company', TextType::class, [
+            'label' => 'Nom de la Société :'
+        ])
+        ->add('name_director', TextType::class, [
+            'label' => 'Nom du directeur :'
+            ])
+        ->add('siret_company', TextType::class, [
+                'label' => 'Siret :'
+                ])
+                ->add('phone_company', TextType::class, [
+                    'label' => 'Téléphone'
+                    ])
+                    ->add('email_company', TextType::class, [
+                        'label' => 'Email'
+                        ])
         ;
     }
 
